6e8f7c2b1ea6ccafea5d1b90b39e65e9f4c36e2d
Manav-NTT <manav12.gupta@nttdata.com>
2024-12-05 11:03:24 +0530

    connected to DB

---
+++ src/main/java/com/learning/quizapp/controller/QuestionController.java
@@ -0,0 +1,23 @@
+package com.learning.quizapp.controller;
+
+import com.learning.quizapp.Question;
+import com.learning.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question") //Question Controller
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions() {
+        return questionService.getAllQuestions();
+    }
+}
